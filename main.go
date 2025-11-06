95d740a7d520e53186b01a9d7717e0a7a40a0aa2
filo13199 <philopateer.youhanna@halan.com>
2025-11-06 18:03:21 +0200

    close range

--- main.go
+++ main.go
@@ -73,7 +73,7 @@ func meansToAnEnd(conn *net.TCPConn, clientId int) {
 			})
 		case "Q":
 			i1 := int(math.Max(0, float64(slices.IndexFunc(data, func(t Tuple) bool {
-				return t.Timestamp > int32(operand1)
+				return t.Timestamp >= int32(operand1)
 			}))))
 
 			i2 := int(math.Max(float64(slices.IndexFunc(data, func(t Tuple) bool {
